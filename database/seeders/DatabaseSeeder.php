d354a6b5d48ff8744737951ee450eae1748b93f6
Eric Escobar Mendoza <eescobar@cdhidalgo.tecnm.mx>
2025-10-07 13:39:35 -0600

    Creación del archivo api para las rutas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Eric Escobar Mendoza',
+            'email' => 'eescobar@laravel.com',
         ]);
 
         User::factory(29)->create();
